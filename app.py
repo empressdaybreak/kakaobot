a3d6d50e13b6054a23cf381f848ff6a752f453bc
DayBreak <as616111@gmail.com>
2017-12-23 16:08:32 +0900

    메시지 답장 기능 추가

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ def keyboard():
         "type" : "buttons",
         "buttons" : ["선택1", "선택2", "선택3"]
     })
+
+@app.route('/message', methods=['POST'])
+def message();
+    return jsonify({
+        'message' : {
+            'text' : '안녕하세요! 아직은 개발 중이라 이 말밖에 못 해요!'
+        }
+    })
