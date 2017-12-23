fa234d3697252da566dc4236e2fdbcc9b0d47842
DayBreak <as616111@gmail.com>
2017-12-23 16:24:50 +0900

    텍스트 방식으로 변경

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 @app.route('/keyboard')
 def keyboard():
     return jsonify({
-        "type" : "buttons",
-        "buttons" : ["선택1", "선택2", "선택3"]
+        "type" : "text"
     })
 
 @app.route('/message', methods=['POST'])
